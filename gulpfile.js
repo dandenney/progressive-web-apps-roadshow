40f754598480d6ddf73d1c14e7862e8cffb867f6
Dan Denney <dd.subscriptions@gmail.com>
2016-08-31 13:50:16 -0500

    Add basic gulp and NPM

---
+++ gulpfile.js
@@ -0,0 +1,64 @@
+/*eslint-env node */
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync').create();
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+
+gulp.task('default', ['copy-html', 'copy-images', 'styles', 'scripts'], function() {
+	gulp.watch('sass/**/*.sass', ['styles']);
+	gulp.watch('js/**/*.js', ['scripts']);
+	gulp.watch('*.html', ['copy-html']);
+	gulp.watch('*.html').on('change', browserSync.reload);
+
+	browserSync.init({
+		server: './dist'
+	});
+});
+
+gulp.task('dist', [
+	'copy-html',
+	'copy-images',
+	'styles',
+	'lint',
+	'scripts',
+	'scripts-dist'
+]);
+
+gulp.task('scripts', function() {
+	gulp.src('js/**/*.js')
+		.pipe(concat('all.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('scripts-dist', function() {
+	gulp.src('js/**/*.js')
+		.pipe(concat('all.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('copy-html', function() {
+	gulp.src('*.html')
+		.pipe(gulp.dest('./dist'));
+});
+
+gulp.task('copy-images', function() {
+	gulp.src('img/*')
+		.pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('styles', function() {
+	gulp.src('sass/**/*.sass')
+		.pipe(sass({
+			outputStyle: 'compressed'
+		}).on('error', sass.logError))
+		.pipe(autoprefixer({
+			browsers: ['last 2 versions']
+		}))
+		.pipe(gulp.dest('dist/styles'))
+		.pipe(browserSync.stream());
+});
